39e0a699f4b7c91c1caf42f47305e5a8e46732dc
apavazza <115418293+apavazza@users.noreply.github.com>
2025-05-10 22:22:03 +0200

    Refactored Footer for improved layout and styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,41 @@
+import Link from "next/link"
+
 export default function Footer() {
     return (
-        <footer className="w-full py-4 mt-auto">
-            <div className="container mx-auto text-center">
-                <p className="text-sm">
-                    Made by <a href="https://amadeopavazza.from.hr/" target="_blank" rel="noreferrer">Amadeo Pavazza</a>
-                </p>
-                <p className="text-xs">
-                    <a href="https://github.com/apavazza/transportation/blob/master/LICENSE" target="_blank" rel="noreferrer">AGPLv3 license</a> • <a href="https://github.com/apavazza/transportation" target="_blank" rel="noreferrer">Repository</a>
-                </p>
+    <footer className="py-4 relative bg-white border-t border-gray-200">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col-reverse lg:flex-row items-center">
+            <div className="space-x-5 lg:absolute lg:left-4">
+                <Link
+                    href={"https://github.com/apavazza/transportation/blob/master/LICENSE"}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-sm text-gray-800 hover:text-gray-500 mb-2 lg:mb-0"
+                >
+                    AGPLv3 license
+                </Link>
+                <Link
+                    href={"https://github.com/apavazza/transportation"}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-sm text-gray-800 hover:text-gray-500 mb-2 lg:mb-0"
+                >
+                    Repository
+                </Link>
             </div>
-        </footer>
-    )
+          <p className="text-sm w-full text-center">
+            &copy; 2025 <Link
+                          href={"https://amadeopavazza.from.hr/"}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="text-sm text-gray-800 hover:text-gray-500 mb-2 lg:mb-0"
+                        >
+                          Amadeo Pavazza
+                        </Link>
+                      . All rights reserved.
+            </p>
+        </div>
+      </div>
+    </footer>
+  )
 }
