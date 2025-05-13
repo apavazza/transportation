ab9b344d20c453c87aa126b044a29f9f8dd53f48
apavazza <115418293+apavazza@users.noreply.github.com>
2025-05-13 23:42:31 +0200

    Updated layout structure to enhance page styling and ensure proper content flow with a main wrapper for children components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
+      <body className="flex flex-col min-h-screen">
+        <main className="flex-grow">
+          {children}
+        </main>
         <Footer />
-        </body>
+      </body>
     </html>
   )
 }
