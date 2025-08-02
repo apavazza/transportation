06bbff9264a073e29f6e1f34b150417fc9d27b19
apavazza <115418293+apavazza@users.noreply.github.com>
2025-08-02 11:52:08 +0200

    Added description metadata and updated dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Footer from '@/src/components/footer'
 
 export const metadata: Metadata = {
   title: 'Transportation Problem Solver',
+  description: "Open-source web app for solving transportation and transshipment problems using North-West Corner, Least Cost, Vogel's Approximation, and MODI (UV) optimization methods. Provides step-by-step allocation and final optimized solutions.",
 }
 
 export default function RootLayout({
